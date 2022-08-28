f988add1ecf53f383b1010ec87a40df2a730a13b
jahantaila <tailajahan@gmail.com>
2022-08-27 22:02:39 -0400

    THIS IS BETTER THAN SATHVIK

--- AstroLite_main/apps.py
+++ AstroLite_main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
+class AstroliteMainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'AstroLite_main'
--- Astrolite/wsgi.py
+++ Astrolite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Astrolite project.
+WSGI config for AstroLite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Astrolite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AstroLite.settings')
 
 application = get_wsgi_application()
