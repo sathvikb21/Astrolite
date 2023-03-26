5a0aa2d6708ed20c98309406dc804143bf3bef32
sathvikbhavaraju <sathvikb21@gmail.com>
2023-03-26 18:42:01 -0400

    Intial Commit

--- Astrolite/wsgi.py
+++ Astrolite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AstroLite project.
+WSGI config for astrolite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AstroLite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrolite.settings')
 
 application = get_wsgi_application()
